280c333626c8772f81f1449b57e65bb8d2cae717
kijana-mungai <njaginmungai@gmail.com>
2025-05-05 14:33:09 +0300

    Add files via upload

---
+++ adultcheck.java
@@ -0,0 +1,11 @@
+public class adultcheck {
+    static int age =17;
+    public static void main(String[] args) {
+        
+        if (age<=17){
+            System.err.println("minor");
+        }else{
+            System.out.println("adult");
+        }
+    }
+}
